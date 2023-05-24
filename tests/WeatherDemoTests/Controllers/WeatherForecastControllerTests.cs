755440ec96442a04870547b7169f2797fdc8d2fb
Dominique St-Amand <dstamand@microsoft.com>
2023-05-24 11:32:46 -0400

    Tweak for integration tests

--- tests/WeatherDemoTests/Controllers/WeatherForecastControllerTests.cs
+++ tests/WeatherDemoTests/Controllers/WeatherForecastControllerTests.cs
@@ -1,36 +1,40 @@
 ﻿using FluentAssertions;
 using WeatherDemo;
-using WeatherDemo.DAL;
 using WeatherDemo.Models;
-using WeatherDemoTests.Helpers;
 
 namespace WeatherDemoTests.Controllers;
 
 public class WeatherForecastControllerTests : IClassFixture<TestWebApplicationFactory<Program>>
 {
     private readonly HttpClient _httpClient;
-    private readonly TestWebApplicationFactory<Program> _factory;
     private static readonly string BASE_URI = "/WeatherForecast";
 
     public WeatherForecastControllerTests(TestWebApplicationFactory<Program> factory)
     {
-        _factory = factory;
-
         // Client that by default follows redirects
-        _httpClient = _factory.CreateClient();
+        _httpClient = factory.CreateClient();
     }
 
     [Theory]
     [InlineData("Montreal")]
+    [Trait("Category", "Integration")]
     public async Task get_city_from_history_should_return_the_expected_result(string city)
     {
-        // Arrange
-
         // Act
-        var act = await _httpClient.GetFromJsonAsync<WeatherAPIResponse>($"{BASE_URI}/history/{city}");
+        var act = await _httpClient.GetFromJsonAsync<WeatherForecastHistory>($"{BASE_URI}/history/{city}");
 
         // Assert
-        var expected = new WeatherForecast();
+        var expected = new WeatherForecastHistory
+        {
+            CityName = "Montreal",
+            Region = "Quebec",
+            Country = "Canada",
+            TimeZoneId = "America/Toronto",
+            Date = new DateTime(2023, 05, 23),
+            Latitude = 45.5,
+            Longitude = -73.58,
+            TemperatureC = 19
+        };
         act.Should().BeEquivalentTo(expected);
     }
 }
--- tests/WeatherDemoTests/TestWebApplicationFactory.cs
+++ tests/WeatherDemoTests/TestWebApplicationFactory.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
 using WeatherDemo.DAL;
-using WeatherDemoTests.Helpers;
 
 namespace WeatherDemoTests;
 
@@ -22,12 +21,11 @@ public class TestWebApplicationFactory<TProgram> : WebApplicationFactory<TProgra
                 var scopedServices = scope.ServiceProvider;
                 var db = scopedServices.GetRequiredService<WeatherDemoDbContext>();
 
+                //var config = scopedServices.GetRequiredService<IConfiguration>();
+                //var dbConfig = config.GetConnectionString("Db");
+
                 // do more checks if EF is responsible to create the db/tables i.e. via db.Database.EnsuredDeleted/Created.
-
-                var config = scopedServices.GetRequiredService<IConfiguration>();
-                var dbConfig = config.GetConnectionString("Db");
-
-                Utilities.InitializeDbForTests(db);
+                //WeatherDemoTests.Helpers.Utilities.InitializeDbForTests(db);
             }
         });
     }
