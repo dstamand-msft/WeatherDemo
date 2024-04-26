39fa4c95caa1629c7fe9d9f7175ae56df2282bbe
Dominique St-Amand <dstamand@microsoft.com>
2024-04-26 18:07:53 -0400

    Add database project to solution and add extra field

---
+++ src/WeatherDemo.Database/dbo/Tables/WeatherHistory.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[WeatherHistory] (
+    [HistoryId]    BIGINT        IDENTITY (1, 1) NOT NULL,
+    [CityName]     VARCHAR (100) NOT NULL,
+    [Region]       VARCHAR (100) NOT NULL,
+    [Country]      VARCHAR (100) NOT NULL,
+    [Longitude]    FLOAT (53)    NOT NULL,
+    [Latitude]     FLOAT (53)    NOT NULL,
+    [TimeZoneId]   VARCHAR (50)  NOT NULL,
+    [Date]         DATE          NOT NULL,
+    [TemperatureC] FLOAT (53)    NOT NULL,
+    [ModifiedDateUTC] DATETIME NOT NULL DEFAULT getutcdate(), 
+    CONSTRAINT [PK_WeatherHistory] PRIMARY KEY CLUSTERED ([HistoryId] ASC)
+);
+
